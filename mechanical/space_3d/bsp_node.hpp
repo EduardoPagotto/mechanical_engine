75dc134bbe8324a60d66c1dac249cd7caf4a529b
Edu Pagotto <edupagotto@gmail.com>
2025-05-09 02:57:19 -0300

    3d space math, space partitions and transformations

---
+++ mechanical/space_3d/bsp_node.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "plane.hpp"
+
+namespace me {
+
+struct BSPTreeNode {
+    Plane hyperPlane; // HyperPlane partition;
+    BSPTreeNode* front;
+    BSPTreeNode* back;
+    uint32_t leafIndex;
+    bool isSolid;
+    bool isLeaf;
+
+    BSPTreeNode(const Plane& _hyperPlane)
+        : hyperPlane(_hyperPlane), front(nullptr), back(nullptr), leafIndex(0), isLeaf(false), isSolid(false) {}
+};
+} // namespace me
